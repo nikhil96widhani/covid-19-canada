c8c9be9d00999b8d2397ca9966a8e98e44eb69c3
Nikhil Widhani <46464842+nikhil96widhani@users.noreply.github.com>
2020-03-29 02:46:05 -0400

    first dash commit

--- app.py
+++ app.py
@@ -1,15 +1,30 @@
-"""Flask App Project."""
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 
-from flask import Flask, jsonify
-app = Flask(__name__)
+dash_app = dash.Dash()
+app = dash_app.server
 
+dash_app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
 
-@app.route('/')
-def index():
-    """Return homepage."""
-    json_data = {'Hello': 'World!'}
-    return jsonify(json_data)
+    html.Div(children='''
+        This is Dash running on Azure App Service without a Docker container.
+    '''),
 
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
 
 if __name__ == '__main__':
-    app.run()
+    dash_app.run_server(debug=True)
